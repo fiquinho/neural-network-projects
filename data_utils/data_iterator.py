e70d630dc92f9a09e9d07a39744040f2093dc146
Federico Bogado <ficobogado@gmail.com>
2018-08-05 20:15:57 -0300

    Added shuffle option and docstrings

--- data_utils/data_iterator.py
+++ data_utils/data_iterator.py
@@ -2,8 +2,22 @@ import numpy as np
 
 
 class DataIterator(object):
+    """
+    Class used to iterate over a dataset that is already converted to numpy arrays. One array
+    should have the input data, and another should have the target data (the expected output).
+    This class allows to iterate the data in batches of ant length.
+    """
 
-    def __init__(self, data: np.array, labels: np.array, batch_size: int):
+    def __init__(self, data: np.array, labels: np.array, batch_size: int, shuffle: bool):
+        """
+        Create a new data iterator, for a specific dataset. The batches are constructed
+        by splitting the data and the labels on the first dimension.
+
+        :param data: An array with the input data.
+        :param labels: An array with the true labels for the input data.
+        :param batch_size: The size of the generated batches.
+        :param shuffle: Activate to shuffle the data before generating the batches.
+        """
 
         if data.shape[0] != labels.shape[0]:
             raise ValueError("The number of data instances in the arrays doesn't match. "
@@ -13,6 +27,7 @@ class DataIterator(object):
         self.data = data
         self.labels = labels
         self.batch_size = batch_size
+        self.shuffle = shuffle
 
         self.data_instances = data.shape[0]
         self.full_batches = self.data_instances // self.batch_size
@@ -22,31 +37,44 @@ class DataIterator(object):
         else:
             self.total_batches = self.full_batches + 1
 
-    def __iter__(self):
+    def __iter__(self) -> (np.array, np.array):
+        """
+        Iterate one time over the entire data, generating batches.
+        This will generate "self.total_batches" number of batches.
 
-        s = np.arange(self.data_instances)
-        np.random.shuffle(s)
+        :return: A tuple with the data batch and the labels batch.
+        """
 
-        self.data = self.data[s]
-        self.labels = self.labels[s]
+        # Shuffle the data before generating the batches, if indicated.
+        if self.shuffle:
+            s = np.arange(self.data_instances)
+            np.random.shuffle(s)
+
+            self.data = self.data[s]
+            self.labels = self.labels[s]
 
         for i in range(self.full_batches):
             data_batch = self.data[i * self.batch_size:(i+1) * self.batch_size, :]
             labels_batch = self.labels[i * self.batch_size:(i+1) * self.batch_size, :]
-            yield i, data_batch, labels_batch
+            yield data_batch, labels_batch
 
         if self.full_batches * self.batch_size != self.data_instances:
             data_batch = self.data[self.full_batches * self.batch_size:, :]
             labels_batch = self.labels[self.full_batches * self.batch_size:, :]
-            yield self.total_batches - 1, data_batch, labels_batch
+            yield data_batch, labels_batch
 
 
-# Test code
-test_data = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
-test_labels = np.array([["a"], ["b"], ["c"], ["d"]])
-print(test_data)
-iterator = DataIterator(test_data, test_labels, 3)
-
-for j in range(2):
-    for batch in iterator:
-        print(batch)
+# # Test code
+# test_data = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
+# test_labels = np.array([[0], [1], [2], [3]])
+# print("Test data:\n{}".format(test_data))
+# print("Test labels:\n{}".format(test_labels))
+#
+# iterator = DataIterator(data=test_data,
+#                         labels=test_labels,
+#                         batch_size=3,
+#                         shuffle=True)
+#
+# for j in range(2):
+#     for batch in iterator:
+#         print(batch)
